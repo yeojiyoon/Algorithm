c570feba2d06e69e35600a39ad76b84ff842661d
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2024-01-08 17:56:07 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int MenOfPassion(int A[], int n)
+{
+	int i;
+	i = n/2;
+	return A[i];
+}
+
+int main(void)
+{
+	long long int n;
+	
+	scanf("%lld", &n);
+	
+	printf("%lld\n2", n*n);
+}
